4843ac063d667751d3013adb5b3c1236cff9b415
Cristovao Iglesias <cristovao.casagrande@gmail.com>
2013-04-14 20:09:46 -0300

    When gender is unknown use 'other' to encode it in PED file. This generate ERROR.

--- public_datas/create_peb.py
+++ public_datas/create_peb.py
@@ -25,18 +25,13 @@ except:
 
 
 
-
-
-
-
-
-
 handle = csv.DictReader(open(file_map, "r"),
         fieldnames=["chromosome", "rs","morgans", "position"],
         delimiter=" ")
 
 
 all_rs=[]  #take all rs
+
 for i in handle:
     all_rs.append(i["rs"])
 all_rs=np.array(all_rs)
@@ -50,117 +45,63 @@ print "phe1\n"
 
 Family_ID=1
 
+def write_ped( dirfilespheno, outputfile, pheno, Family_ID):
+    """
+    """
+    for i in os.listdir(dirfilespheno):
+        all_rs_ind_tmp = np.array(["0 0"]*len(all_rs), dtype='S3') #initialaze every genotype with "0 0"    
 
-for i in os.listdir(dir_files_phenotype1):
+        sex=""
+        print "\nParse the file ", dirfilespheno+"/"+i,
+        if "XY" in i:
+            sex="1"        
+        elif "XX" in i:
+            sex="2"       
+        else:
+            sex="other"        
+        try:
+            snps = sn.parse(dirfilespheno+"/"+i)       #take another files
+        except:
+            print  "   ERRO 1"
+            continue 
 
-    all_rs_ind_tmp = np.array(["0 0"]*len(all_rs), dtype='S3') #initialaze every genotype with "0 0"    
-   # print len(all_rs_ind_tmp),'initialaze array with every genotype with "0 0"'    
+        all_names=[]
+        all_gen=[]
 
-    sex=""
-
-    print "\nParse the file ", dir_files_phenotype1+"/"+i,
-
-
-    if "XY" in i:
-        sex="1"
-        
-    elif "XX" in i:
-        sex="2"
-        
-    else:
-        sex="other"
-        
+        try:
+            for cur_snp in snps:
+                if len(cur_snp.genotype)==2 and cur_snp.genotype in valid_alleles:# "--" and cur_snp.genotype != "II" and cur_snp.genotype != "DI":
+                    all_names.append(cur_snp.name)
+                    all_gen.append("%s %s" % (cur_snp.genotype[0], cur_snp.genotype[1]))
+        except:
+            print  "   ERRO 2"
+            continue
     
-    try:	
-        snps = sn.parse(dir_files_phenotype1+"/"+i)       #take another files
-    except:
-        print  "   ERRO 1"
-        continue      
-
-
-    all_names=[]
-    all_gen=[]
-    try:
-        for cur_snp in snps:
-            if len(cur_snp.genotype)==2 and cur_snp.genotype in valid_alleles:# "--" and cur_snp.genotype != "II" and cur_snp.genotype != "DI":
-                all_names.append(cur_snp.name)
-                all_gen.append("%s %s" % (cur_snp.genotype[0], cur_snp.genotype[1]))
-    except:
-        print  "   ERRO 2"
-        continue
-
-    print "   OK"    
-
-    idx = np.flatnonzero(np.in1d(all_rs, np.array(all_names))) #return the index of the elements of "all_names" in "all_rs"
-    all_rs_ind_tmp[idx] = np.array(all_gen)
-
-   
+        print "   OK" 
+        idx = np.flatnonzero(np.in1d(all_rs, np.array(all_names)))
+        all_rs_ind_tmp[idx] = np.array(all_gen)   
     
+        outputfile.write( str(Family_ID)+" "+ "1 0 0 "+sex+"  "+ pheno+"  ")
+        for i in all_rs_ind_tmp:
+                outputfile.write(i+"  ")
+        outputfile.write("\n")
+        Family_ID=Family_ID+1
 
 
-    ofile.write( str(Family_ID)+" "+ "1 0 0 "+sex+"  1  ")
-    for i in all_rs_ind_tmp:
-            ofile.write(i+"  ")
-    ofile.write("\n")
-    Family_ID=1+Family_ID
-
-
-
-
+write_ped( dir_files_phenotype1, ofile, "1",Family_ID)
 
 print "phe2\n"
 
-
-
-for i in os.listdir(dir_files_phenotype2):
-
-    all_rs_ind_tmp = np.array(["0 0"]*len(all_rs), dtype='S3') #initialaze every genotype with "0 0"    
-    #print len(all_rs_ind_tmp),'initialaze array with every genotype with "0 0"'    
-
-
-    sex=""
-
-    print "\nParse the file ", dir_files_phenotype2+"/"+i,
-
-    if "XY" in i:
-        sex="1"
-        
-    elif "XX" in i:
-        sex="2"
-       
-    else:
-        sex="other"
-        
-    try:
-        snps = sn.parse(dir_files_phenotype2+"/"+i)       #take another files
-    except:
-        print  "   ERRO 1"
-        continue 
-
-    all_names=[]
-    all_gen=[]
-
-    try:
-        for cur_snp in snps:
-            if len(cur_snp.genotype)==2 and cur_snp.genotype in valid_alleles:# "--" and cur_snp.genotype != "II" and cur_snp.genotype != "DI":
-                all_names.append(cur_snp.name)
-                all_gen.append("%s %s" % (cur_snp.genotype[0], cur_snp.genotype[1]))
-    except:
-        print  "   ERRO 2"
-        continue
-
-    print "   OK" 
-    idx = np.flatnonzero(np.in1d(all_rs, np.array(all_names)))
-    all_rs_ind_tmp[idx] = np.array(all_gen)
-  
-   
-
-    ofile.write( str(Family_ID)+" "+ "1 0 0 "+sex+"  2  ")
-    for i in all_rs_ind_tmp:
-            ofile.write(i+"  ")
-    ofile.write("\n")
-    Family_ID=1+Family_ID
-
-
+write_ped( dir_files_phenotype2, ofile, "2",Family_ID)
 
 ofile.close()
+
+
+
+    
+    
+    
+
+
+
+
